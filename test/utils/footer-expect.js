7b9ce42966445a109434dee3e7efc7ba41a3bd4e
John Watson <john.watson1@defra.gov.uk>
2026-04-08 17:39:49 +0100

    Update tests

--- test/utils/footer-expect.js
+++ test/utils/footer-expect.js
@@ -1,6 +1,7 @@
 import { expect } from 'vitest'
 
 const expectedFooterLinks = [
+  'https://www.gov.uk/government/organisations/department-for-environment-food-rural-affairs',
   'https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/',
   'https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/'
 ]
